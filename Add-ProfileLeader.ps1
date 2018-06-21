bd49164b4d653cf7c7a7c8ef3ae3ab15c6baa254
Spencer Harbar <spence@harbar.net>
2018-06-21 23:00:32 +0100

    Add-ProfileLeader

---
+++ Add-ProfileLeader.ps1
@@ -0,0 +1,13 @@
+﻿<#
+    Adds a user as a "Profile Leader" and displays the results
+#>
+If ((Get-PSSnapin -Name "Microsoft.SharePoint.PowerShell" -EA 0) -eq $null) { Add-PSSnapin -Name "Microsoft.SharePoint.PowerShell" }
+
+$Account = "FABRIKAM\administrator"
+$UpaProxyName = "User Profile Service Application Proxy"
+$UpaProxy = get-spserviceapplicationproxy  | Where-Object {$_.Name -eq $UpaProxyName}
+
+Add-SPProfileLeader $UpaProxy $Account
+
+Get-SPProfileLeader $UpaProxy
+#EOF
