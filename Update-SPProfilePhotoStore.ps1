09a1cb25e39ce27b5cfa3a011e47251b96c3f6a6
Spencer Harbar <spence@harbar.net>
2018-06-21 22:47:48 +0100

    Update-SPProfilePhotoStore

---
+++ Update-SPProfilePhotoStore.ps1
@@ -0,0 +1,13 @@
+﻿<#
+    Creates the three thumbnail photos from source binary data already in My Site host.
+
+    Should be scheduled to run on a machine in the farm hosting the User Profiles service instance
+    If run on a machine not hosting that service, it will complete successfully, doing nothing.
+#>
+If ((Get-PSSnapin -Name "Microsoft.SharePoint.PowerShell" -EA 0) -eq $null) { Add-PSSnapin -Name "Microsoft.SharePoint.PowerShell" }
+
+
+$MySiteHost = "https://onedrive.fabrikam.com"
+
+Update-SPProfilePhotoStore -CreateThumbnailsForImportedPhotos $true -MySiteHostLocation $MySiteHost
+#EOF
