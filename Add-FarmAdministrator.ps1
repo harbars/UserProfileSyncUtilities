e4a2f2f8c36f641e9ca7793a7d60f76763c6fd04
Spencer Harbar <spence@harbar.net>
2018-06-21 22:40:36 +0100

    Add-FarmAdministrator

---
+++ Add-FarmAdministrator.ps1
@@ -0,0 +1,16 @@
+<#
+    Adds a user to Farm Administrators, and outputs the resulting Farm Administrators.
+
+    The account which runs the SharePoint MA must be a farm administrator in order to connect to Central Administration
+#>
+If ((Get-PSSnapin -Name "Microsoft.SharePoint.PowerShell" -EA 0) -eq $null) { Add-PSSnapin -Name "Microsoft.SharePoint.PowerShell" }
+
+$FarmAdmin = "FABRIKAM\spma"
+
+$CentralAdminUrl = Get-SPWebApplication -includecentraladministration | Where-Object {$_.IsAdministrationWebApplication} | Select-Object -ExpandProperty Url
+$CentralAdminSite = Get-SPWeb -Identity $CentralAdminUrl
+$AdminGroup = $CentralAdminSite.AssociatedOwnerGroup
+Set-SPUser -UserAlias $FarmAdmin -Web $CentralAdminUrl -Group $AdminGroup #New-SPUser if never added before
+
+$AdminGroup.Users
+#EOF
